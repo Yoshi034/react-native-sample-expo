ac999b0a0920902860a94b59adab2784e45877bf
tie304368 <yoshiwara.masashi@intra.aws-axion.net>
2022-04-19 15:32:14 +0900

    画像表示

--- Sample/App.tsx
+++ Sample/App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <View style={styles.moziBox}>
+          <Text style={styles.text}>
+            かわいい犬の画像です。柴犬が飼いたいです。
+          </Text>
+          <Text style={styles.subText}>さくら</Text>
+        </View>
+        <View style={styles.gazoBox}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: "https://picsum.photos/id/237/200/300" }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,5 +27,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    height: 100,
+    width: "100%",
+    borderColor: "lightblue",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  moziBox: {
+    flex: 1,
+    backgroundColor: "steelblue",
+    padding: 16,
+    justifyContent: "space-between",
+  },
+  gazoBox: {
+    width: 100,
+    backgroundColor: "powderblue",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "red",
   },
 });
